ec574efc355a456a859e5959cc6c0733b3570ffc
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-17 18:56:29 +0300

    Add field sizing style to textarea

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         <form className="flex flex-col gap-4">
           <textarea
             aria-label="Write comment"
-            className="border-grey-100 text-grey-800 placeholder-grey-500 min-h-24 w-full resize-none rounded-lg border px-6 py-2.5 outline-none focus:border-purple-600"
+            className="border-grey-100 text-grey-800 placeholder-grey-500 field-sizing-content max-h-47.5 min-h-24 w-full resize-none rounded-lg border px-6 py-2.5 outline-none focus:border-purple-600"
             placeholder="Add a comment..."
           ></textarea>
 
