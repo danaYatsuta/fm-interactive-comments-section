6f943b5322475eb88894f81afc338a4614303fd8
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-18 20:06:00 +0300

    Return base text area component (smh)

---
+++ app/components/base-text-area.tsx
@@ -0,0 +1,13 @@
+export default function BaseTextArea({
+  placeholder,
+}: {
+  placeholder?: string;
+}) {
+  return (
+    <textarea
+      aria-label="Comment content"
+      className="border-grey-100 text-grey-800 placeholder-grey-500 field-sizing-content max-h-47.5 min-h-24 w-full resize-none rounded-lg border px-6 py-2.5 outline-none focus:border-purple-600"
+      placeholder={placeholder}
+    ></textarea>
+  );
+}
