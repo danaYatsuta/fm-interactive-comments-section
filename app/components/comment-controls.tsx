8bacb0161f21a1b30a670b14402394ef8d223245
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-18 21:13:17 +0300

    Move comment controls to a separate component

---
+++ app/components/comment-controls.tsx
@@ -0,0 +1,62 @@
+import IconButton from "@/app/components/icon-button";
+import IconMinus from "@/app/components/icon-minus";
+import IconPlus from "@/app/components/icon-plus";
+import iconDelete from "@/public/icons/icon-delete.svg";
+import iconEdit from "@/public/icons/icon-edit.svg";
+import iconReply from "@/public/icons/icon-reply.svg";
+
+export default function CommentControls({
+  canUserEdit,
+  onDeleteClick,
+  onEditClick,
+  onReplyClick,
+  score,
+}: Readonly<{
+  canUserEdit: boolean;
+  onDeleteClick: () => void;
+  onEditClick: () => void;
+  onReplyClick: () => void;
+  score: number;
+}>) {
+  return (
+    <div className="flex items-center justify-between text-purple-200">
+      <div className="bg-grey-50 flex h-10 w-25 rounded-xl text-lg font-bold">
+        <button
+          aria-label="Upvote"
+          className="w-10 rounded-xl -outline-offset-2 outline-purple-600 hover:text-purple-600"
+          type="button"
+        >
+          <IconPlus />
+        </button>
+
+        <p className="grow self-center text-center text-base font-medium text-purple-600">
+          <span className="sr-only">Score: </span>
+          {score}
+        </p>
+
+        <button
+          aria-label="Downvote"
+          className="w-10 rounded-xl -outline-offset-2 outline-purple-600 hover:text-purple-600"
+          type="button"
+        >
+          <IconMinus />
+        </button>
+      </div>
+
+      {canUserEdit ? (
+        <div className="flex gap-4">
+          <IconButton
+            color="pink"
+            icon={iconDelete}
+            onClick={onDeleteClick}
+            text="Delete"
+          />
+
+          <IconButton icon={iconEdit} onClick={onEditClick} text="Edit" />
+        </div>
+      ) : (
+        <IconButton icon={iconReply} onClick={onReplyClick} text="Reply" />
+      )}
+    </div>
+  );
+}
--- app/components/comment.tsx
+++ app/components/comment.tsx
@@ -7,15 +7,10 @@ import TimeAgo from "react-timeago";
 import BaseButton from "@/app/components/base-button";
 import BaseCard from "@/app/components/base-card";
 import BaseTextArea from "@/app/components/base-text-area";
+import CommentControls from "@/app/components/comment-controls";
 import CommentForm from "@/app/components/comment-form";
 import DeleteDialog from "@/app/components/delete-dialog";
-import IconButton from "@/app/components/icon-button";
-import IconMinus from "@/app/components/icon-minus";
-import IconPlus from "@/app/components/icon-plus";
 import timeAgoFormatter from "@/app/lib/timeAgoFormatter";
-import iconDelete from "@/public/icons/icon-delete.svg";
-import iconEdit from "@/public/icons/icon-edit.svg";
-import iconReply from "@/public/icons/icon-reply.svg";
 
 export default function Comment({
   commentData,
@@ -168,53 +163,13 @@ export default function Comment({
                 {commentData.content}
               </p>
 
-              <div className="flex items-center justify-between text-purple-200">
-                <div className="bg-grey-50 flex h-10 w-25 rounded-xl text-lg font-bold">
-                  <button
-                    aria-label="Upvote"
-                    className="w-10 rounded-xl -outline-offset-2 outline-purple-600 hover:text-purple-600"
-                    type="button"
-                  >
-                    <IconPlus />
-                  </button>
-
-                  <p className="grow self-center text-center text-base font-medium text-purple-600">
-                    <span className="sr-only">Score: </span>
-                    {commentData.score}
-                  </p>
-
-                  <button
-                    aria-label="Downvote"
-                    className="w-10 rounded-xl -outline-offset-2 outline-purple-600 hover:text-purple-600"
-                    type="button"
-                  >
-                    <IconMinus />
-                  </button>
-                </div>
-
-                {canUserEdit ? (
-                  <div className="flex gap-4">
-                    <IconButton
-                      color="pink"
-                      icon={iconDelete}
-                      onClick={handleDeleteClick}
-                      text="Delete"
-                    />
-
-                    <IconButton
-                      icon={iconEdit}
-                      onClick={onEditClick}
-                      text="Edit"
-                    />
-                  </div>
-                ) : (
-                  <IconButton
-                    icon={iconReply}
-                    onClick={onReplyClick}
-                    text="Reply"
-                  />
-                )}
-              </div>
+              <CommentControls
+                canUserEdit={canUserEdit}
+                onDeleteClick={handleDeleteClick}
+                onEditClick={onEditClick}
+                onReplyClick={onReplyClick}
+                score={commentData.score}
+              />
             </>
           )}
         </article>
