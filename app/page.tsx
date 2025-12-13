6bf2ae24cffd4fe851922594221b8dd3aa4e1a4e
Dana Yatsuta <danayatsuta@gmail.com>
2025-12-13 22:25:11 +0300

    Print error instead of something went wrong

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import { fetchComments } from "@/app/lib/data";
 export default async function App() {
   const comments = await fetchComments();
 
-  if (comments instanceof Response) return <p>Something went wrong</p>;
+  if (comments instanceof Response) {
+    const json = await comments.json();
+
+    return <p>{JSON.stringify(json)}</p>;
+  }
 
   return <TemporaryClientComponent comments={comments} />;
 }
